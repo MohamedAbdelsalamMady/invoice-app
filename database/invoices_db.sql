cd20921a381c513d212ddfd1f159e56c492b895e
MohamedAbdelsalamMady <156349834+MohamedAbdelsalamMady@users.noreply.github.com>
2024-08-24 20:05:49 +0300

    Update invoices_db.sql

--- database/invoices_db.sql
+++ database/invoices_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 22, 2024 at 01:59 PM
+-- Generation Time: Aug 24, 2024 at 05:03 PM
 -- Server version: 5.7.36
 -- PHP Version: 8.1.0
 
@@ -36,7 +36,15 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `contact_phone` varchar(15) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`client_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `name`, `address`, `contact_email`, `contact_phone`, `created_at`) VALUES
+(2, 'Ahmed Hussien', 'Mahrm Beh', 'ahmed.hussien12@gmail.com', '012846531', '2024-08-23 23:56:29'),
+(3, 'Ali Elsayed', 'Miami', 'ali123@gmail.com', '0128469856', '2024-08-24 16:13:46');
 
 -- --------------------------------------------------------
 
@@ -56,7 +64,15 @@ CREATE TABLE IF NOT EXISTS `invoices` (
   PRIMARY KEY (`invoice_id`),
   UNIQUE KEY `invoice_number` (`invoice_number`),
   KEY `client_id` (`client_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`invoice_id`, `client_id`, `invoice_number`, `invoice_date`, `due_date`, `total_amount`, `created_at`) VALUES
+(1, 2, 'INV-66c9a4348322e', '2024-08-22', '2031-10-21', 100000.00, '2024-08-24 09:13:24'),
+(2, 3, 'INV-66ca071277666', '2024-08-14', '2032-10-18', 102000.00, '2024-08-24 16:15:14');
 
 -- --------------------------------------------------------
 
@@ -75,7 +91,18 @@ CREATE TABLE IF NOT EXISTS `invoice_items` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `invoice_id` (`invoice_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `invoice_items`
+--
+
+INSERT INTO `invoice_items` (`id`, `invoice_id`, `description`, `quantity`, `price_per_unit`, `total`, `created_at`) VALUES
+(1, 1, 'GOOD TV', 10, 5000.00, 50000.00, '2024-08-24 09:13:24'),
+(2, 1, 'phone', 5, 10000.00, 50000.00, '2024-08-24 09:13:24'),
+(3, 2, 'Good Tv', 10, 5000.00, 50000.00, '2024-08-24 16:15:14'),
+(4, 2, 'Phone', 5, 10000.00, 50000.00, '2024-08-24 16:15:14'),
+(5, 2, 'Table', 10, 200.00, 2000.00, '2024-08-24 16:15:14');
 
 -- --------------------------------------------------------
 
@@ -92,7 +119,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_role`, `password`, `created_at`) VALUES
+(1, 'Mooo', 'mo123@gmail.com', 'admin', '$2y$10$1NzcrmNnM4F/TS1.uU1swOqhJXrEjM1xGRe5QwzG7BsKXQMfzUsCa', '2024-08-23 19:03:32'),
+(3, 'Ali', 'ali123@gmail.com', 'user', '$2y$10$Jg0HgnGbkVrqNB.wmPDideUFBQAvV9NTN8S1phGPqVRS.7/2oh8Ii', '2024-08-24 16:12:13');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
